99cf759cf5e8df731fe9d43e508827380573175a
Chebolu Srinivas <Chebolu.Srinivas@coforge.com>
2025-09-01 23:26:36 +0530

    code changes

--- tests/pwlocators.spec.ts
+++ tests/pwlocators.spec.ts
@@ -9,4 +9,28 @@ test('Verify Playwright Locators', async ({page})=>
     //Get by ALT Text Locator
     const logo:Locator = page.getByAltText("nopCommerce demo store");
     await expect(logo).toBeVisible();
+
+    //Get By Text Locator
+    const registerLink:Locator = page.getByText("Welcome to our store");
+    await expect(registerLink).toBeVisible();
+
+    //Get by Role Locator
+    await page.getByRole('link', {name: 'Register'}).click();
+    await expect(page.getByRole('heading', {name: 'Register'})).toBeVisible();
+    
+    //Get by label Locator
+    const firstName:Locator = page.getByLabel("First name");
+    await firstName.fill("John");
+    await expect(firstName).toHaveValue("John");
+
+    //Get by Title Locator
+    const lastName:Locator = page.getByTitle("Last name");
+    await lastName.fill("Wick");
+    await expect(lastName).toHaveValue("Wick");
+
+    //get by test id locator
+    const newsLetter:Locator = page.getByTestId("Newsletter");
+    await newsLetter.check();
+    await expect(newsLetter).toBeChecked();
 });
+
